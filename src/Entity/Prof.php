9f67ce3e0e5cd978074ef1c0a9f758736c3c5114
Brewen06 <brewen68@ik.me>
2025-10-16 16:42:14 +0200

    Entités + CRUD

---
+++ src/Entity/Prof.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+
+#[ORM\Table(name:"tbl_prof")]
+#[ORM\Entity(repositoryClass: ProfRepository::class)]
+class Prof
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $motdepasse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMotdepasse(): ?string
+    {
+        return $this->motdepasse;
+    }
+
+    public function setMotdepasse(string $motdepasse): static
+    {
+        $this->motdepasse = $motdepasse;
+
+        return $this;
+    }
+}
